41c63205b61ce22ffd4d9d8498fb0c2cb67f9e4e
KateyKwesi <kateykwesi001@gmail.com>
2025-10-02 15:11:45 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener(`DOMContentLoaded`, addTask);
+const addButton = document.getElementById(`add-task-btn`);
+const taskInput = document.getElementById(`task-input`);
+const taskList = document.getElementById(`task-list`);
+
+function addTask() {
+  let taskText = taskInput.value.trim();
+  taskText === `` ? alert(`Please enter a task`) : null;
+
+  let li = document.createElement(`li`);
+  li.textContent = taskText;
+  let btnRemove = document.createElement(`button`);
+  btnRemove.textContent = `Remove`;
+  btnRemove.classList.add = `remove-btn`;
+  btnRemove.addEventListener(`click`, () => {
+    li.remove();
+  });
+  li.appendChild(btnRemove);
+  taskList.appendChild(li);
+  taskInput.value = ``;
+}
+
+addButton.addEventListener(`click`, addTask);
+
+taskInput.addEventListener(`keypress`, (e) => {
+  if (e.key === `Enter`) {
+    addTask();
+  }
+});
